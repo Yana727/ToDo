7f56e9a039b402abbb4f72b54bf19e2df83a4c38
Yana Wilkins <yav9774@gmail.com>
2017-08-28 13:50:12 -0400

    contex and database

---
+++ Models/toDoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ToDo.Models; //add this here 
+
+namespace ToDo.Models
+{
+    public partial class toDoContext : DbContext
+    {
+        public DbSet<ToDoModel> ToDoModel { get; set; } //gives the name of the table 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@"Host=localhost;Database=toDo;Username=dev;Password=yanamac");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
---
+++ Models/toDoModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ToDo.Models
+{
+    public class ToDoModel
+    {
+        public string TaskName { get; set; }
+        public bool Complete { get; set; } = false;
+        public DateTime Time { get; set; } = DateTime.Now;
+
+        public int ID {get; set; }
+    }
+}
